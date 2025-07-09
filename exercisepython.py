bf4aaebec42938e4a9ae0c4004de71a7fe8ecd42
aditya <adityadubey2281@gmail.com>
2025-07-08 22:40:52 -0700

    Update exercisepython.py

--- exercisepython.py
+++ exercisepython.py
@@ -8,5 +8,26 @@ print(age)
 print(height)
 print(is_student)
 
+# ✅ Task 1: Create variables for your city (string), number of kids (int), and has_pet (boolean)
+city = 'Vancouver'
+hobby = 'reading books'
+car = False
+
+print(city)
+print(hobby)
+print(car)
+
+# ▶️ 2. Reassigning Variables
+age = 24 #happy birthday!
+print("\nNew age after birthday:", age)
+
+# ✅ Task 2: Change the value of your city variable to another city and print it
+city = "Toronto"
+print("New city:", city)
+
+
+# ▶️ 3. Using Variables in Expressions
+year_born = 2025 - age
+print("\nYear born:", year_born)
 
 
